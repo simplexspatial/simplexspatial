51abb02f356b88169096d881087b601d89a8ad2e
Angel Cervera Claudio <angelcervera@gmail.com>
2019-02-12 22:05:17 +0000

    Adding edges

--- src/main/scala/com/simplexportal/spatial/RTreeActor.scala
+++ src/main/scala/com/simplexportal/spatial/RTreeActor.scala
@@ -5,7 +5,7 @@
 package com.simplexportal.spatial
 
 import akka.actor.{Actor, ActorLogging, Props}
-import com.simplexportal.spatial.RTreeActor.{AddNode, ConnectNodes, GetNode}
+import com.simplexportal.spatial.RTreeActor._
 
 object RTreeActor {
 
@@ -15,6 +15,10 @@ object RTreeActor {
   case class AddNode(node: Model.Node) extends RTreeActions
   case class GetNode(id: Long) extends RTreeActions
   case class ConnectNodes(edge: Model.Edge) extends RTreeActions
+  object GetMetrics extends RTreeActions
+
+  sealed  trait  RTreeMessages
+  case class Metrics(nodes: Long, edges: Long)
 
 }
 
@@ -29,7 +33,11 @@ class RTreeActor(boundingBox: Model.BoundingBox) extends Actor with ActorLogging
     ins: Set[Long] = Set.empty
   )
 
-  protected case class Edge(attributes: Model.Attributes)
+  protected case class Edge(
+    source: Long,
+    target: Long,
+    attributes: Model.Attributes
+  )
 
   // TODO: For performance, should be replaced by a binary tree or mutable Map ??
 
@@ -44,16 +52,22 @@ class RTreeActor(boundingBox: Model.BoundingBox) extends Actor with ActorLogging
     case GetNode(id) =>
       sender ! nodes.get(id).map(node => reconstructNode(id, node))
 
+    case GetMetrics =>
+      sender ! Metrics(nodes.size, edges.size)
+
     case AddNode(modelNode) =>
       addNode(modelNode.id, Node(modelNode.location, modelNode.attributes))
 
     case ConnectNodes(modelEdge) =>
+
       // Add "out" connection.
-      def addOut = nodes.get(modelEdge.source).map(n => addNode(modelEdge.id, n.copy(outs = n.outs + modelEdge.id)))
+      val addedOut = nodes.get(modelEdge.source).map(n => addNode(modelEdge.source, n.copy(outs = n.outs + modelEdge.id))).isDefined
+
       // Add "in" connection.
-      def addIn = nodes.get(modelEdge.target).map(n => addNode(modelEdge.id, n.copy(ins = n.ins + modelEdge.id)))
+      val addedIn = nodes.get(modelEdge.target).map(n => addNode(modelEdge.target, n.copy(ins = n.ins + modelEdge.id))).isDefined
+
       // Add edge data only if the node is present as In or Out.
-      if(addOut.isDefined || addIn.isDefined) addEdge(modelEdge.id, Edge(modelEdge.attributes))
+      if(addedOut || addedIn) addEdge(modelEdge.id, Edge(modelEdge.source, modelEdge.target, modelEdge.attributes))
 
   }
 
@@ -61,7 +75,18 @@ class RTreeActor(boundingBox: Model.BoundingBox) extends Actor with ActorLogging
 
   private def addEdge(id: Long, edge: Edge) = edges = edges + ( id -> edge )
 
-  private def reconstructNode(id: Long, node: Node) = Model.Node(id, node.location, node.attributes) // TODO: Fill with Edges data.
+  private def reconstructEdge(id: Long, edge: Edge) =
+    Model.Edge(id, edge.source, edge.target, edge.attributes)
 
+  private def reconstructNode(id: Long, node: Node) =
+    Model.Node(
+      id,
+      node.location,
+      node.attributes,
+      reconstructEdgeList(node.ins) ++ reconstructEdgeList(node.outs)
+    )
+
+  private def reconstructEdgeList(ids: Set[Long]) =
+    ids.flatMap(edgeId => edges.get(edgeId).map(reconstructEdge(edgeId, _)))
 
 }
--- src/test/scala/com/simplexportal/spatial/RTreeActorSpec.scala
+++ src/test/scala/com/simplexportal/spatial/RTreeActorSpec.scala
@@ -7,7 +7,7 @@ package com.simplexportal.spatial
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
 import com.simplexportal.spatial.Model.{BoundingBox, Edge, Location, Node}
-import com.simplexportal.spatial.RTreeActor.{AddNode, ConnectNodes, GetNode}
+import com.simplexportal.spatial.RTreeActor._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
@@ -29,34 +29,62 @@ class RTreeActorSpec extends TestKit(ActorSystem("RTreeActorSpec"))
       rTreeActor ! AddNode(Node(10, Location(5,5), Map(10L -> "17 Redwood Avenue")))
 
       rTreeActor ! GetNode(10)
-      expectMsg(100.millis, Some(Node(10, Location(5,5), Map(10L -> "17 Redwood Avenue"))))
+      rTreeActor ! GetMetrics
+
+      expectMsg(Some(Node(10, Location(5,5), Map(10L -> "17 Redwood Avenue"))))
+      expectMsg(Metrics(1, 0))
     }
 
     "connect nodes adding edge information" when {
       "source and target are present" in {
         val rTreeActor = system.actorOf(RTreeActor.props(BoundingBox(Location(1,1), Location(10,10))))
-        rTreeActor ! AddNode(Node(10, Location(5,5), Map(10L -> "17 Redwood Avenue")))
-        rTreeActor ! AddNode(Node(11, Location(5,6), Map(10L -> "19 Redwood Avenue")))
+        rTreeActor ! AddNode(Node(10, Location(5,5), Map(10L -> "source node")))
+        rTreeActor ! AddNode(Node(11, Location(5,6), Map(10L -> "target node")))
         rTreeActor ! ConnectNodes(Edge(1L, 10L, 11L, Map(1L -> "bridge") ))
 
         rTreeActor ! GetNode(10)
         rTreeActor ! GetNode(11)
+        rTreeActor ! GetMetrics
 
         val expectedEdge = Edge(1, 10, 11, Map(1L -> "bridge"))
-        expectMsg(100.millis, Some(Node(10, Location(5,5), Map(10L -> "17 Redwood Avenue"), Set(expectedEdge))))
-        expectMsg(100.millis, Some(Node(11, Location(5,6), Map(10L -> "19 Redwood Avenue"), Set(expectedEdge))))
+        expectMsg(100.millis, Some(Node(10, Location(5,5), Map(10L -> "source node"), Set(expectedEdge))))
+        expectMsg(100.millis, Some(Node(11, Location(5,6), Map(10L -> "target node"), Set(expectedEdge))))
+        expectMsg(Metrics(2, 1))
       }
 
       "only source is present" in {
+        val rTreeActor = system.actorOf(RTreeActor.props(BoundingBox(Location(1,1), Location(10,10))))
+        rTreeActor ! AddNode(Node(10, Location(5,5), Map(10L -> "source node")))
+        rTreeActor ! ConnectNodes(Edge(1L, 10L, 11L, Map(1L -> "bridge") ))
 
+        rTreeActor ! GetNode(10)
+        rTreeActor ! GetMetrics
+
+        val expectedEdge = Edge(1, 10, 11, Map(1L -> "bridge"))
+        expectMsg(100.millis, Some(Node(10, Location(5,5), Map(10L -> "source node"), Set(expectedEdge))))
+        expectMsg(Metrics(1, 1))
       }
 
       "only target is present" in {
+        val rTreeActor = system.actorOf(RTreeActor.props(BoundingBox(Location(1,1), Location(10,10))))
+        rTreeActor ! AddNode(Node(11, Location(5,6), Map(10L -> "target node")))
+        rTreeActor ! ConnectNodes(Edge(1L, 10L, 11L, Map(1L -> "bridge") ))
 
+        rTreeActor ! GetNode(11)
+        rTreeActor ! GetMetrics
+
+        val expectedEdge = Edge(1, 10, 11, Map(1L -> "bridge"))
+        expectMsg(100.millis, Some(Node(11, Location(5,6), Map(10L -> "target node"), Set(expectedEdge))))
+        expectMsg(Metrics(1, 1))
       }
 
       "neither target nor source are present" in {
+        val rTreeActor = system.actorOf(RTreeActor.props(BoundingBox(Location(1,1), Location(10,10))))
+        rTreeActor ! ConnectNodes(Edge(1L, 10L, 11L, Map(1L -> "bridge") ))
 
+        rTreeActor ! GetMetrics
+
+        expectMsg(Metrics(0, 0))
       }
 
     }
