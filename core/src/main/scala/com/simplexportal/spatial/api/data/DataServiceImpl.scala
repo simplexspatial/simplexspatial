ab0d040a5035f697fd32270401ec525ba03dee9e
Angel Cervera Claudio <angelcervera@gmail.com>
2019-09-01 20:03:14 +0100

    Streaming commands. Keep only block streaming.

--- core/src/main/scala/com/simplexportal/spatial/api/data/DataServiceImpl.scala
+++ core/src/main/scala/com/simplexportal/spatial/api/data/DataServiceImpl.scala
@@ -46,39 +46,6 @@ class DataServiceImpl(tile: ActorRef)(
     Future.successful(Done())
   }
 
-//  // sent from actor to stream to "ack" processing of given element
-//  val AckMessage = Done
-//
-//  // sent from stream to actor to indicate start, end or failure of stream:
-//  val InitMessage = TileActor.StreamInitialized
-//  val OnCompleteMessage = TileActor.StreamCompleted
-//  val onErrorMessage = (ex: Throwable) => TileActor.StreamFailure(ex)
-//
-//  val sink = Sink.actorRefWithAck(
-//    tile,
-//    onInitMessage = InitMessage,
-//    ackMessage = AckMessage,
-//    onCompleteMessage = OnCompleteMessage,
-//    onFailureMessage = onErrorMessage)
-
-  override def streamCommands(in: Source[ExecuteCmd, NotUsed]): Future[Done] =
-    in
-      .mapAsync(4)( // TODO: Maybe better mapAsync ???
-        executeCmd =>
-          executeCmd.command match {
-            case ExecuteCmd.Command.Way(way) => Future(AddWay(way.id, way.nodeIds, way.attributes))
-            case ExecuteCmd.Command.Node(node) => Future(AddNode(node.id, node.lat, node.lon, node.attributes))
-            case ExecuteCmd.Command.Empty => Future(null)
-          }
-      )
-      .groupedWithin(300, 1 second)
-//      .map(tile ! AddBatch(_))
-//      .runWith(sink)
-      .runForeach( cmds => {
-        tile ! AddBatch(cmds)
-      })
-      .map(_ => Done())
-
   override def streamBatchCommands(in: Source[ExecuteBatchCmd, NotUsed]): Source[Done, NotUsed] =
       in
         .map( executeBatchCmd => executeBatchCmd.commands.flatMap(
@@ -94,18 +61,6 @@ class DataServiceImpl(tile: ActorRef)(
           Done()
           }
         )
-
-  override def executeBatch(in: ExecuteBatchCmd): Future[Done] = {
-    val commands: Seq[TileCommands] = in.commands.flatMap( executeCmd =>
-        executeCmd.command match {
-          case ExecuteCmd.Command.Way(way) => Some(AddWay(way.id, way.nodeIds, way.attributes))
-          case ExecuteCmd.Command.Node(node) => Some(AddNode(node.id, node.lat, node.lon, node.attributes))
-          case ExecuteCmd.Command.Empty => None
-        }
-    )
-    tile ! AddBatch(commands)
-    Future.successful(Done())
-  }
 
   override def getMetrics(in: GetMetricsCmd): Future[Metrics] = {
     (tile ask GetMetrics)
