c6edbd16d9196b5fe5dabec0888e82e96aedc1b4
Angel Cervera Claudio <293444+angelcervera@users.noreply.github.com>
2021-01-26 08:34:34 +0000

    Latest versions

--- core/src/main/scala/com/simplexportal/spatial/index/grid/entrypoints/restful/RestServer.scala
+++ core/src/main/scala/com/simplexportal/spatial/index/grid/entrypoints/restful/RestServer.scala
@@ -191,7 +191,7 @@ object RestServer extends Directives with RestfulJsonProtocol {
           case grid.GridNearestNodeReply(Right(nodes)) =>
             complete(NearestNodes(nodes.map(n => Node(n.id, n.location.lat, n.location.lon, n.attributes))))
         }
-        parameters(('lat.as[Double], 'lon.as[Double])) { (lat, lon) =>
+        parameters('lat.as[Double], 'lon.as[Double]) { (lat, lon) =>
           reply(
             gridIndex
               .ask[grid.GridNearestNodeReply](ref => grid.GridNearestNode(model.Location(lat, lon), ref)),
